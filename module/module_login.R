8110c803153c879265b0fd17775969a353f34da4
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2021-10-13 15:07:38 -0300

    first commit

---
+++ module/module_login.R
@@ -0,0 +1,59 @@
+
+# UI ----
+login_ui <- function(id, title) {
+  
+  ns <- NS(id)
+   
+  div(
+    id = ns("login"),
+    style = "width: 500px; max-width: 100%; margin: 0 auto; padding: 20px;",
+    div(
+      class = "well",
+      h2(class = "text-center", title),
+      
+      textInput(
+        inputId = ns("user_name"),
+        label = tagList(icon("user"), "User Name"),
+        placeholder = "Enter user name"
+      ),
+      
+      passwordInput(
+        inputId = ns("password"),
+        label = tagList(icon("unlock-alt"), "Password"),
+        placeholder = "Enter password"
+      ),
+      
+      div(
+        class = "text-center",
+        actionButton(
+          inputId = ns("login_button"), "Log in",
+          class = "btn-primary",
+          style = "color:white"
+        )
+      )
+    )
+  ) 
+}
+
+# server ----
+validate_pwd <- function(input, output, session, data, user_col, pwd_col) {
+  
+  user <- data |> pull(!!enquo(user_col))
+  pwd <- data |> pull(!!enquo(pwd_col))
+  
+  eventReactive(input$login_button, {
+    
+    validate <- FALSE
+    if (input$user_name == user && input$password == pwd) {
+      validate <- TRUE
+    }
+    
+    if (validate) {
+      shinyjs::hide(id = "login")
+    }
+    
+    validate
+  })
+}
+
+
---
+++ module_training.R
@@ -0,0 +1,167 @@
+# BUSINESS SCIENCE ----
+# DS4B 202-R ----
+# AUTHENTICATION & MODULE TRAINING -----
+# Version 1
+
+# APPLICATION DESCRIPTION ----
+# - Render a Login Dialog Page
+# - Dynamically render a UI upon authentication
+# - Create a module to produce the login
+# - Use the shinyauthr package
+
+library(shiny)
+library(shinythemes)
+library(tidyverse)
+library(shinyjs)
+
+library(shinyauthr) # devtools::install_github("business-science/shinyauthr")
+source("module/module_login.R")
+
+ui <- navbarPage(
+    title = "Module Training", 
+    theme = shinytheme("flatly"),
+    collapsible = TRUE,
+    
+    tabPanel(
+        useShinyjs(),
+        title = "Login Module",
+        shinyauthr::loginUI(id = "login_3", error_message = "Wrong password"),
+        verbatimTextOutput(outputId = "creds"),
+        uiOutput(outputId = "web_page")
+       
+     )
+)
+
+server <- function(input, output, session) {
+    
+    user_base_tbl <- tibble(
+        user_name = "user1",
+        password  = "pass1"
+    )
+    
+    # NO MODULE ----
+    validate <- FALSE
+    
+    validate_password <- eventReactive(input$login_button, {
+      if (input$user_name == user_base_tbl$user_name && input$password == user_base_tbl$password) {
+        validate <- TRUE
+      }
+      
+      validate
+    })
+    
+    output$display_content <- renderUI({
+      req(validate_password())
+      
+      div(
+        class = "well",
+        id = "success",
+        h1(class = "page-header", "Stock Analyser",
+           tags$small("by Business Science")),
+        p(class = "lead", "Page content...")
+      )
+    })
+    
+    
+    # MODULE ----
+    validate_password_2 <- callModule(
+      module   = validate_pwd, 
+      id       = "login_2",
+      data     = user_base_tbl,
+      user_col = user_name,
+      pwd_col  = password
+    )
+    
+    output$display_content_2 <- renderUI({
+      
+      req(validate_password_2())
+      
+      div(
+        class = "well",
+        id = "success",
+        h1(class = "page-header", "Stock Analyser {VALIDATE MODULE}",
+           tags$small("by Business Science")),
+        p(class = "lead", "Page content...")
+      )
+    })
+    
+    # SHINYAUTHR ----
+    credentials <- callModule(
+      module = shinyauthr::login,
+      id = "login_3",
+      data = user_base_tbl,
+      user_col = user_name,
+      pwd_col = password,
+      log_out = reactive(logout_init())
+    )
+    
+    user_auth <- reactive({
+      credentials()$user_auth
+    })
+    
+    user_data <- reactive({
+      credentials()$info
+    })
+    
+    logout_init <- callModule(
+      module = shinyauthr::logout,
+      id = "logout",
+      active= reactive(user_auth())
+    )
+    
+    output$creds <- renderPrint({
+      credentials()
+    })
+    
+    output$web_page <- renderUI({
+      
+      req(user_auth())
+      
+      tagList(
+        h2("No Module"),
+        
+        div(
+          id = "login",
+          style = "width: 500px; max-width: 100%; margin: 0 auto; padding: 20px;",
+          div(
+            class = "well",
+            h2(class = "text-center", "Please Login"),
+            
+            textInput(
+              inputId = "user_name",
+              label = tagList(icon("user"), "User Name"),
+              placeholder = "Enter user name"
+            ),
+            
+            passwordInput(
+              inputId = "password",
+              label = tagList(icon("unlock-alt"), "Password"),
+              placeholder = "Enter password"
+            ),
+            
+            div(
+              class = "text-center",
+              actionButton(
+                inputId = "login_button", "Log in",
+                class = "btn-primary",
+                style = "color:white"
+              )
+            )
+          )
+        ),
+        
+        uiOutput(outputId = "display_content"),
+        
+        h2("Using A Module"),
+        
+        login_ui(id = "login_2", "Modular Login"), 
+        uiOutput(outputId = "display_content_2"),
+        
+        h2('Using Shiny Auth')
+      )
+
+    })
+    
+}
+
+shinyApp(ui, server)
